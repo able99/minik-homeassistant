13dfab07dc426b28ff568c24e5d2d21f4d5ab964
able99 <8846755@qq.com>
2018-04-16 20:44:52 +0800

    feature: new docker installer

--- homeassistant_raspberry.sh
+++ homeassistant_raspberry.sh
@@ -1,3 +1,5 @@
+# 1. curl -sL https://raw.githubusercontent.com/able99/minik-homeassistant/master/homeassistant_raspberry.sh | nohup sh & ; tail -f nohup.out
+
 # evn
 # ====================
 PATH_HA_RUN=~/ha
@@ -12,20 +14,8 @@ mkdir -p ${PATH_HA_RUN}/share/tmp
 # docker
 # ====================
 echo "------------- install docker ------------"
-
-# deps
-apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     gnupg2 \
-     software-properties-common -y
-
 # install
-curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | apt-key add -
-echo "deb [arch=armhf] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
-apt-get update
-apt-get install docker-ce -y
+curl -fsSL https://get.docker.com | bash -s docker --mirror Aliyun
 #usermod -aG docker $USER
 
 # config
--- system_config_raspberry.sh
+++ system_config_raspberry.sh
@@ -1,25 +1,20 @@
 # 1. download image https://www.raspberrypi.org/downloads/raspbian/
-# 2. download image tool https://etcher.io/
-# 3. flash
+# 2. download tool https://etcher.io/
+# 3. flash image
 # 4. touch ssh on /boot
-# 5. from '/boot/config.txt' add 'audio_pwm_mode=2' under 'dtparam=audio=on'
+# 5. from '/boot/config.txt' add 'audio_pwm_mode=2' under 'dtparam=audio=on' and add gpu_mem=16  
 # 6. boot
-# 7. ssh
-# 8. sudo su
-# 9. curl
+# 7. sudo su && cd ~ && curl -sL https://raw.githubusercontent.com/able99/minik-homeassistant/master/system_config_raspberry.sh | sh 
+
 
 # source
 # ========================
 echo "------------- config source list for soft ------------"
 
-echo "
-deb http://mirrors.aliyun.com/raspbian/raspbian/  stretch main non-free contrib
-deb-src http://mirrors.aliyun.com/raspbian/raspbian/  stretch main non-free contrib
-" > /etc/apt/sources.list
+echo "deb http://mirrors.aliyun.com/raspbian/raspbian/  stretch main non-free contrib
+deb-src http://mirrors.aliyun.com/raspbian/raspbian/  stretch main non-free contrib" > /etc/apt/sources.list
 
-echo "
-deb http://mirrors.aliyun.com/debian/ stretch main ui
-" > /etc/apt/sources.list.d/raspi.list
+#echo "deb http://mirrors.aliyun.com/debian/ stretch main ui" > /etc/apt/sources.list.d/raspi.list
 
 apt-get update
 apt-get upgrade -y
@@ -42,11 +37,11 @@ echo 'PermitRootLogin without-password' >> /etc/ssh/sshd_config
 
 mkdir -p ~/.ssh/
 chmod 700 ~/.ssh/
-touch authorized_keys
+touch ~/.ssh/authorized_keys
 chmod 600 ~/.ssh/authorized_keys 
 
 echo 'input key for root ssh (or just enter without input)'
-read sshkey
+read SSHKEY
 if [ -n "$SSHKEY" ]; then
     echo "$SSHKEY" >> ~/.ssh/authorized_keys
 fi
